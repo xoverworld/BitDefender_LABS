0f6b96ffb823b90e49981da6bb76e6a3834be579
ChaymmoXD <patrick.sami2001@gmail.com>
2026-04-20 17:04:20 +0300

    start parsing labyrinth

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -2,20 +2,38 @@ use std::env;
 use std::fs::File;
 use std::io::{Read, Seek};
 use std::path::Path;
+use serde::Deserialize;
+use std::num::ParseIntError;
+use serde_json::Result;
 
-fn main(){
-    let args: Vec<String> = env::args().collect();
-
-    let aux = &args[1];
-    let path = Path::new(aux);
-    println!("path: {}",path.display());
-    let zip_file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", path.display(), why),
-        Ok(file) => file,
-    };
-
-    list_zip_contents(zip_file);
+#[derive(Debug, Deserialize)]
+struct GridCell {
+    x: usize,
+    y: usize
 }
+
+#[derive(Debug, Deserialize)]
+struct Labyrinth
+{
+    width: usize,
+    height: usize,
+    start: GridCell,
+    goal: GridCell,
+    grid: Vec<GridCell>
+}
+// fn main(){
+//     let args: Vec<String> = env::args().collect();
+
+//     let aux = &args[1];
+//     let path = Path::new(aux);
+//     println!("path: {}",path.display());
+//     let zip_file = match File::open(&path) {
+//         Err(why) => panic!("couldn't open {}: {}", path.display(), why),
+//         Ok(file) => file,
+//     };
+
+//     list_zip_contents(zip_file);
+// }
 
 fn list_zip_contents(reader: impl Read + Seek) -> zip::result::ZipResult<()> {
     let mut zip = zip::ZipArchive::new(reader)?;
@@ -25,5 +43,22 @@ fn list_zip_contents(reader: impl Read + Seek) -> zip::result::ZipResult<()> {
             println!("{}", file.name());
         }
     }
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    let aux = &args[1];
+    let path = Path::new(aux);
+    let labyrinth_file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", path.display(), why),
+        Ok(file) => file,
+    };
+    let labyrinth: Labyrinth = serde_json::from_reader(labyrinth_file)?;
+
+    // println!("start {}, {}", labyrinth.start.x, labyrinth.start.y);
+
+    //parse the grid vector
     Ok(())
 }
